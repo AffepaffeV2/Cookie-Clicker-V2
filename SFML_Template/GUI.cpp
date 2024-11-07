6fa1df24ef373104144f21c05c44f86fcb0a7dbb
AffepaffeV2 <te22ah9@cng.se>
2024-11-07 10:07:12 +0100

    Small Fixes
    Some small changes made in the label for the cookies

--- SFML_Template/GUI.cpp
+++ SFML_Template/GUI.cpp
@@ -8,22 +8,13 @@ GUI::~GUI()
 {
 }
 
-// Updates the UI
-void GUI::updategui()
-{
-}
-
-// Renders the UI
-void GUI::rendergui()
-{
-}
 
 void GUI::print_someting(const std::string& text)
 {
     std::cout << text;
 }
 
-void GUI::guicreator2(tgui::GuiBase& gui, const std::string& ButtonText, const std::string& PrintText, tgui::Layout sizeWidth, tgui::Layout sizeHeight, tgui::Layout posX, tgui::Layout posY)
+void GUI::guicreator2(tgui::GuiBase& gui, const std::string ButtonText, const std::string PrintText, tgui::Layout sizeWidth, tgui::Layout sizeHeight, tgui::Layout posX, tgui::Layout posY)
 {
     auto button = tgui::Button::create(ButtonText);
 
@@ -41,15 +32,17 @@ void GUI::guicreator2(tgui::GuiBase& gui, const std::string& ButtonText, const s
 
 void GUI::staticguicreator(tgui::GuiBase& gui, const std::string& ButtonText, tgui::Layout sizeWidth, tgui::Layout sizeHeight, tgui::Layout posX, tgui::Layout posY)
 {
-    auto button = tgui::Button::create(ButtonText);
-
+    auto label = tgui::Label::create(ButtonText);
+    
     // Set the size of the label
-    button->setSize({ sizeWidth, sizeHeight });
+    label->setSize({ sizeWidth, sizeHeight });
 
     // Set the position of the label
-    button->setPosition({ posX, posY });
+    label->setPosition({ posX, posY });
 
-    gui.add(button);
+    label->getRenderer()->setTextColor(tgui::Color(255, 255, 255));
+
+    gui.add(label);
 }
 // Public function to initialize the GUI
 bool GUI::RunGUI(tgui::GuiBase& gui)
@@ -57,7 +50,7 @@ bool GUI::RunGUI(tgui::GuiBase& gui)
     try
     {
         guicreator2(gui, "Click Me", "Button clicked!", "50%, 16.67%", "25%, 70%");
-        staticguicreator(gui, "Cookies: 0","50%", "16,67%", "70%", "25%");
+        staticguicreator(gui, "Cookies: 0", "75%", "25%", "0%", "0%");
         return true;
     }
     catch (const tgui::Exception& e)
@@ -65,4 +58,12 @@ bool GUI::RunGUI(tgui::GuiBase& gui)
         std::cerr << "Failed to load TGUI widgets: " << e.what() << std::endl;
         return false;
     }
+}
+
+// Updates the UI
+void GUI::updategui()
+{
+
+
+
 }
--- SFML_Template/GUI.h
+++ SFML_Template/GUI.h
@@ -17,13 +17,12 @@ public:
 
 private:
     // Accept width and height as separate Layout parameters
-    void guicreator2(tgui::GuiBase& gui, const std::string& ButtonText, const std::string& PrintText, tgui::Layout sizeWidth = "50%", tgui::Layout sizeHeight = "16.67%", tgui::Layout posX = "25%", tgui::Layout posY = "70%");
+    void guicreator2(tgui::GuiBase& gui, const std::string ButtonText, const std::string PrintText, tgui::Layout sizeWidth = "50%", tgui::Layout sizeHeight = "16.67%", tgui::Layout posX = "25%", tgui::Layout posY = "70%");
 
-    void staticguicreator(tgui::GuiBase& gui, const std::string& ButtonText,tgui::Layout sizeWidth, tgui::Layout sizeHeight, tgui::Layout posX, tgui::Layout posY);
+    void staticguicreator(tgui::GuiBase& gui, const std::string& ButtonText, tgui::Layout sizeWidth, tgui::Layout sizeHeight, tgui::Layout posX, tgui::Layout posY );
  
     void print_someting(const std::string& text);
 
     void updategui();
 
-    void rendergui();
 };
